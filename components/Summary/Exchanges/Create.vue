f4fe305ef4cfe5ce6cc079f89227d1fa57fdfabe
PedGarBlue <pedro.alejandro.g@hotmail.com>
2023-04-14 23:16:03 -0400

    refactor(ExchangesCreate): Replaced raw form with Form Added Notifications on Form submit finish

--- components/Summary/Exchanges/Create.vue
+++ components/Summary/Exchanges/Create.vue
@@ -1,16 +1,8 @@
 <script lang="ts" setup>
-import VueDatePicker from '@vuepic/vue-datepicker'
-import { format } from 'date-fns'
-import { createExchange } from '@/api/exchanges'
-import { AccountComplete, useAccounts } from '~~/stores/accounts'
+import { createExchange } from '~~/api/exchanges'
 import Modal from '~~/components/Modal.vue'
-
-enum LoadingStatus {
-  IDLE = 'IDLE',
-  LOADING = 'LOADING',
-  FINISHED = 'FINISHED',
-  ERROR = 'ERROR',
-}
+import { useAccounts } from '~~/stores/accounts'
+import { useNotificationsStore } from '~~/stores/notifications'
 
 const emits = defineEmits(['form-finished'])
 
@@ -19,56 +11,84 @@ const { t } = useLang()
 const modal = ref<InstanceType<typeof Modal> | null>(null)
 const accountsState = useAccounts()
 const accountsList = accountsState.getAccounts
-const defaultData = {
-  description: '',
-  date: new Date(),
-  fromAmount: '',
-  toAmount: '',
-}
-const transactionData = reactive({ ...defaultData })
-const accountFrom = ref<AccountComplete>(accountsList[0])
-const accountTo = ref<AccountComplete>(accountsList[1])
-const loadingState = ref<LoadingStatus>(LoadingStatus.IDLE)
+const notifications = useNotificationsStore()
+
+const fields = [
+  {
+    key: 'date',
+    title: 'Date',
+    default: new Date(),
+    datePicker: true,
+    value: '',
+  },
+  {
+    key: 'description',
+    title: 'Description',
+    default: '',
+    value: '',
+  },
+  {
+    key: 'from_account',
+    title: 'Sender Account',
+    default: accountsList[0],
+    selectOptions: accountsList,
+    selectionKey: 'name',
+    optionKey: 'name',
+    value: '',
+  },
+  {
+    key: 'from_amount',
+    title: 'Sender Amount',
+    default: 0,
+    value: 0,
+    componentProps: {
+      type: 'number',
+      required: true,
+    },
+  },
+  {
+    key: 'to_account',
+    title: 'Recipient Account',
+    default: accountsList[1],
+    selectOptions: accountsList,
+    selectionKey: 'name',
+    optionKey: 'name',
+    value: '',
+  },
+  {
+    key: 'to_amount',
+    title: 'Recipient Amount',
+    default: 0,
+    value: 0,
+    componentProps: {
+      type: 'number',
+      required: true,
+    },
+  },
+]
 
 // methods
 const openModal = () => {
   modal.value?.open()
 }
 const closeModal = () => {
-  clearForm()
   modal.value?.close()
 }
-const clearForm = () => {
-  transactionData.description = defaultData.description
-  transactionData.date = defaultData.date
-  accountFrom.value = accountsList[0]
-  accountTo.value = accountsList[1]
-  transactionData.fromAmount = defaultData.fromAmount
-  transactionData.toAmount = defaultData.toAmount
-  loadingState.value = LoadingStatus.IDLE
+const submit = (data: any) => {
+  return createExchange({
+    ...data,
+    from_account: data.from_account.id,
+    to_account: data.to_account.id,
+  })
 }
 
-const addExchange = async () => {
-  if (loadingState.value !== LoadingStatus.IDLE) return
-  const { description, date, fromAmount, toAmount } = transactionData
-  const data = {
-    description,
-    date: format(new Date(date), 'yyyy-MM-dd'),
-    from_account: accountFrom.value.id,
-    to_account: accountTo.value.id,
-    from_amount: fromAmount,
-    to_amount: toAmount,
-  }
-
-  loadingState.value = LoadingStatus.LOADING
-
-  try {
-    await createExchange(data)
-    loadingState.value = LoadingStatus.FINISHED
-    emits('form-finished')
-  } catch (error) {
-    loadingState.value = LoadingStatus.ERROR
-  }
+const finishCreate = () => {
+  notifications.addNotification({
+    type: 'success',
+    text: t('pages.summary.exchanges.create.success'),
+  })
+  closeModal()
+  emits('form-finished')
 }
 </script>
 
@@ -78,102 +98,17 @@ const addExchange = async () => {
       t('pages.summary.exchanges.create.title')
     }}</Button>
     <Modal ref="modal">
-      <template #header>
-        <h3>
-          {{ t('pages.summary.exchanges.create.title') }}
-        </h3>
-        {{ transactionData }}
-      </template>
-      <template #body>
-        <template v-if="loadingState === LoadingStatus.LOADING">
-          <div class="flex w-full h-full items-center justify-center p-16">
-            <IconMdi:loading class="text-[5rem] animate-spin" />
-          </div>
-        </template>
-        <template v-else-if="loadingState === LoadingStatus.FINISHED">
-          <div class="flex w-full h-full items-center justify-center p-16">
-            <IconMdi:checkbox-marked-circle
-              class="text-[5rem] text-green-500"
-            />
-          </div>
-        </template>
-        <div v-else class="flex flex-col gap-2">
-          <div>
-            <div class="text-base">Date</div>
-            <VueDatePicker v-model="transactionData.date" />
-          </div>
-          <FormTextInput
-            v-model="transactionData.description"
-            size="md"
-            class="md:1/3"
-            title="Description"
-          />
-          <!-- Account From  -->
-          <Select
-            v-model="accountFrom"
-            :options="accountsList"
-            title="Sender Account"
-          >
-            <template #selected-value="{ selected }">
-              {{ selected ? selected.name : '' }}
-            </template>
-            <template #item-value="{ option }">
-              {{ option.name }}
-            </template>
-          </Select>
-          <FormTextInput
-            v-model="transactionData.fromAmount"
-            type="number"
-            size="md"
-            class="md:1/3"
-            title="Sender Account Amount"
-          >
-            <template #prefix>
-              {{ accountFrom.currencyData.name }}
-            </template>
-          </FormTextInput>
-
-          <!-- Account To  -->
-          <Select
-            v-model="accountTo"
-            :options="accountsList"
-            title="Recipient Account"
-          >
-            <template #selected-value="{ selected }">
-              {{ selected ? selected.name : '' }}
-            </template>
-            <template #item-value="{ option }">
-              {{ option.name }}
-            </template>
-          </Select>
-          <FormTextInput
-            v-model="transactionData.toAmount"
-            type="number"
-            size="md"
-            class="md:1/3"
-            title="Recipient Account Amount"
-          >
-            <template #prefix>
-              {{ accountTo.currencyData.name }}
-            </template>
-          </FormTextInput>
-        </div>
-      </template>
-      <template #footer>
-        <div
-          v-if="loadingState === LoadingStatus.FINISHED"
-          class="flex flex-row gap-6"
-        >
-          <Button @click="() => clearForm()">
-            {{ t('pages.summary.exchanges.create.restart') }}
-          </Button>
-          <Button @click="() => closeModal()">
-            {{ t('others.close') }}
-          </Button>
-        </div>
-        <Button v-else @click="() => addExchange()">{{
-          t('pages.summary.exchanges.create.title')
-        }}</Button>
+      <template #contents>
+        <Form
+          class="m-auto max-w-1/3"
+          :fields="fields"
+          :form-submit-fn="submit"
+          :form-title="t('pages.summary.exchanges.create.title')"
+          :restart-title="t('pages.summary.exchanges.create.restart')"
+          :submit-title="t('pages.summary.exchanges.create.title')"
+          :close-title="t('others.close')"
+          @form-finished="() => finishCreate()"
+        ></Form>
       </template>
     </Modal>
   </div>
