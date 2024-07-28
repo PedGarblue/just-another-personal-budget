6ece1e4b66b25bea9caab234fbc0c2fef178de45
PedGarBlue <pedro.alejandro.g@hotmail.com>
2024-07-28 17:43:34 -0400

    fix: wrong use of public runtine env variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      apiUrl: process.env.API_URL || 'http://localhost:3000',
+      apiUrl: '',
       token: {
         accessExpires: 60 * 60 * 1000, // 1 hour
         refreshExpires: 24 * 3600 * 1000, // 1 day
