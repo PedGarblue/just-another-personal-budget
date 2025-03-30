3145f4a3b1ff45668f58441510ea65f3dee849cf
PedGarBlue <pedro.alejandro.g@hotmail.com>
2025-03-30 16:22:19 -0400

    fix: missing lang keys and migrate locales files to json

--- composables/useLang.ts
+++ composables/useLang.ts
@@ -2,7 +2,5 @@ import { useI18n } from 'vue-i18n'
 
 export const useLang = () => {
   const { t } = useI18n()
-  return {
-    t,
-  }
+  return { t }
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,7 @@ export default defineNuxtConfig({
     '~/plugins/navbar.ts',
     '~/plugins/vue3-easy-data-table.ts',
     '~/plugins/chartjs.ts',
+    '~/plugins/i18n.ts',
   ],
 
   // build
@@ -43,7 +44,6 @@ export default defineNuxtConfig({
   // modules
   modules: [
     'unplugin-icons/nuxt',
-    '@intlify/nuxt3',
     '@pinia/nuxt',
     '@nuxt/content',
     '@vueuse/nuxt',
@@ -72,16 +72,6 @@ export default defineNuxtConfig({
     // global transition
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
-  },
-
-  // localization - i18n config
-  intlify: {
-    localeDir: 'locales',
-    vueI18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      availableLocales: ['en', 'es'],
-    },
   },
 
   // vueuse
